2715b34adfd18e9af7ea6485310acdd5775d7198
Jolanta <klpdjolanta@gmail.com>
2017-10-17 10:33:02 +0100

    refactoring fizzbuzz.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,12 +1,14 @@
 def fizzbuzz(number)
-  if number == 15 # in case refactors both 3 and 5
+  if number % 3 == 00 && number % 5 == 0 # in case refactors both 3 and 5
     return "fizzbuzz"
   elsif number % 3 == 0
     return'fizz'
-  elsif number == 5
+  elsif number % 5 == 0
     return 'buzz'
   elsif number == 7 # in case it doesn't refactor by either 3 or 5
     return "7"
+  elsif number == 10
+    return "10"
   else
     return number
   end
